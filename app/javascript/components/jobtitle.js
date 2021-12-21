ac022adbc079a2e8b5e87141a099945c72eaedcc
remi <remi.tondeux@gmail.com>
2021-12-21 11:09:12 +0100

    add banner and links with anchors

--- app/javascript/components/jobtitle.js
+++ app/javascript/components/jobtitle.js
@@ -5,7 +5,7 @@ const DynamicJobTitle = () => {
   const jobTitle = document.querySelector('#job-title');
   if (jobTitle) {
     new Typed('#job-title', {
-      strings: ["Developer Fullstack Freelance ^1700", "Ruby/Rails/JavaScript/HTML/CSS ^2000"],
+      strings: ["Developer Fullstack Freelance ^1700", "Ruby - Rails - JavaScript - HTML - CSS ^2000"],
       typeSpeed: 60,
       showCursor: false,
       loop: true
