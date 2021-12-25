0bf1eb2d993cc8959c9c02dd2687b50aadd8045e
remi <remi.tondeux@gmail.com>
2021-12-25 10:31:04 +0100

    working on project's cards 60%

--- app/javascript/controllers/projects_controller.js
+++ app/javascript/controllers/projects_controller.js
@@ -4,6 +4,13 @@ export default class extends Controller {
   static targets = ['card']
 
   return(event) {
-    console.log(event);
+    if (this.cardTarget.matches('.reset')) {
+      this.cardTarget.classList.add("flip-card");
+      this.cardTarget.classList.remove("reset");
+    }
+    else {
+      this.cardTarget.classList.add("reset");
+      this.cardTarget.classList.remove("flip-card");
+    }
   }
 }
