95e91ec21fa24e23a07a6b5bd00c891b4f0f97a5
remi <remi.tondeux@gmail.com>
2021-12-28 17:09:40 +0100

    carroussel 15%

--- app/javascript/controllers/carroussel_controller.js
+++ app/javascript/controllers/carroussel_controller.js
@@ -4,7 +4,26 @@ export default class extends Controller {
   static targets = ['card']
 
   turnLeft(event) {
-    console.log("turning left !")
+    this.cardTargets.forEach( card => {
+      if (card.classList.contains("card-left")) {
+        card.classList.remove("card-left");
+        card.classList.add("d-none");
+        console.log(card.outerHTML)
+        // rebalancer la card à la droite
+        card.remove();
+    } else if (card.classList.contains("main-testimonial")) {
+        card.classList.remove("main-testimonial");
+        card.classList.add("testimonial");
+        card.classList.add("card-left");
+      }
+    else if (card.classList.contains("card-right")) {
+        card.classList.add("main-testimonial");
+        card.classList.remove("testimonial");
+        card.classList.remove("card-right");
+      }
+    });
+    this.cardTargets[2].classList.remove("d-none");
+    this.cardTargets[2].classList.add("card-right");
   }
 
   turnRight(event) {
