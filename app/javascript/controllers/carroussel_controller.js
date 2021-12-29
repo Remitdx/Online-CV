e46901d01de40a4d7c4594b550713c76d35f5a6b
remi <remi.tondeux@gmail.com>
2021-12-29 10:13:27 +0100

    carroussel 90%

--- app/javascript/controllers/carroussel_controller.js
+++ app/javascript/controllers/carroussel_controller.js
@@ -5,47 +5,60 @@ export default class extends Controller {
 
   turnLeft(event) {
     this.cardTargets.forEach( card => {
-      if (card.classList.contains("card-left")) {
-        card.classList.remove("card-left");
+      if (card.classList.contains("card-left-out")) {
+        card.classList.remove("card-left-out");
         card.classList.add("d-none");
         card.parentNode.insertAdjacentHTML("beforeend", card.outerHTML);
         // rebalancer la card à la droite
         card.remove();
       }
-      else if (card.classList.contains("main-testimonial")) {
-        card.classList.remove("main-testimonial");
-        card.classList.add("testimonial");
+      else if (card.classList.contains("card-left")) {
+        card.classList.remove("card-left");
+        card.classList.add("card-left-out");
+      }
+      else if (card.classList.contains("main")) {
+        card.classList.remove("main");
         card.classList.add("card-left");
       }
       else if (card.classList.contains("card-right")) {
-        card.classList.add("main-testimonial");
-        card.classList.remove("testimonial");
+        card.classList.add("main");
         card.classList.remove("card-right");
       }
+      else if (card.classList.contains("card-right-out")) {
+        card.classList.remove("card-right-out");
+        card.classList.add("card-right");
+      }
+
     });
-    this.cardTargets[2].classList.remove("d-none");
-    this.cardTargets[2].classList.add("card-right");
+    this.cardTargets[4].classList.remove("d-none");
+    this.cardTargets[4].classList.add("card-right-out");
   }
 
   turnRight(event) {
     this.cardTargets.forEach( card => {
-      if (card.classList.contains("card-right")) {
-        card.classList.remove("card-right");
+      if (card.classList.contains("card-right-out")) {
+        card.classList.remove("card-right-out");
         card.classList.add("d-none");
       }
-      else if (card.classList.contains("main-testimonial")) {
-        card.classList.remove("main-testimonial");
-        card.classList.add("testimonial");
+      else if (card.classList.contains("card-right")) {
+        card.classList.remove("card-right");
+        card.classList.add("card-right-out");
+      }
+      else if (card.classList.contains("main")) {
+        card.classList.remove("main");
         card.classList.add("card-right");
       }
       else if (card.classList.contains("card-left")) {
-        card.classList.add("main-testimonial");
-        card.classList.remove("testimonial");
+        card.classList.add("main");
         card.classList.remove("card-left");
+      }
+      else if (card.classList.contains("card-left-out")) {
+        card.classList.remove("card-left-out");
+        card.classList.add("card-left");
       }
     });
     this.cardTargets[6].classList.remove("d-none");
-    this.cardTargets[6].classList.add("card-left");
+    this.cardTargets[6].classList.add("card-left-out");
     this.cardTargets[6].parentNode.insertAdjacentHTML("afterbegin", this.cardTargets[6].outerHTML);
     // rebalancer la card à la droite
     this.cardTargets[7].remove();
