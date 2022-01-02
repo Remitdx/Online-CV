dde633a643593bf231943f90169b067c77b603b0
remi <remi.tondeux@gmail.com>
2022-01-02 17:07:38 +0100

    game 95%

--- app/javascript/controllers/game_controller.js
+++ app/javascript/controllers/game_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ['hint','mode', 'avatar', 'card', 'tag', 'carrou', 'contact', 'green', 'red', 'percent']
+  static targets = ['hint','mode', 'avatar', 'card', 'tag', 'carrou', 'contact', 'green', 'red', 'percent', 'end']
 
   hint(event) {
     this.hintTarget.classList.toggle("d-none");
@@ -14,9 +14,9 @@ export default class extends Controller {
       this.modeTarget.innerHTML = '<i class="fas fa-check"></i>';
       const regexp = /\d+/ ;
       const percent = this.greenTarget.outerHTML.match(regexp)[0];
-      this.greenTarget.outerHTML = `<div class="green" data-game-target="green" style="width: ${parseInt(percent, 10) + 15}%;"></div>`
-      this.redTarget.outerHTML = `<div class="red" data-game-target="red" style="width: ${100-(parseInt(percent, 10) + 15)}%;"></div>`
-      this.percentTarget.innerHTML = `${parseInt(percent, 10)+15}%`;
+      this.greenTarget.outerHTML = `<div class="green" data-game-target="green" style="width: ${parseInt(percent, 10) + 18}%;"></div>`
+      this.redTarget.outerHTML = `<div class="red" data-game-target="red" style="width: ${100-(parseInt(percent, 10) + 18)}%;"></div>`
+      this.percentTarget.innerHTML = `${parseInt(percent, 10)+ 18}%`;
     }
   }
 
@@ -40,9 +40,9 @@ export default class extends Controller {
       this.cardTarget.innerHTML = '<i class="fas fa-check"></i>';
       const regexp = /\d+/;
       const percent = this.greenTarget.outerHTML.match(regexp)[0];
-      this.greenTarget.outerHTML = `<div class="green" data-game-target="green" style="width: ${parseInt(percent, 10) + 15}%;"></div>`
-      this.redTarget.outerHTML = `<div class="red" data-game-target="red" style="width: ${100 - (parseInt(percent, 10) + 15)}%;"></div>`
-      this.percentTarget.innerHTML = `${parseInt(percent, 10) + 15}%`;
+      this.greenTarget.outerHTML = `<div class="green" data-game-target="green" style="width: ${parseInt(percent, 10) + 17}%;"></div>`
+      this.redTarget.outerHTML = `<div class="red" data-game-target="red" style="width: ${100 - (parseInt(percent, 10) + 17)}%;"></div>`
+      this.percentTarget.innerHTML = `${parseInt(percent, 10) + 17}%`;
     }
   }
 
@@ -53,9 +53,34 @@ export default class extends Controller {
       this.carrouTarget.innerHTML = '<i class="fas fa-check"></i>';
       const regexp = /\d+/;
       const percent = this.greenTarget.outerHTML.match(regexp)[0];
-      this.greenTarget.outerHTML = `<div class="green" data-game-target="green" style="width: ${parseInt(percent, 10) + 15}%;"></div>`
-      this.redTarget.outerHTML = `<div class="red" data-game-target="red" style="width: ${100 - (parseInt(percent, 10) + 15)}%;"></div>`
-      this.percentTarget.innerHTML = `${parseInt(percent, 10) + 15}%`;
+      this.greenTarget.outerHTML = `<div class="green" data-game-target="green" style="width: ${parseInt(percent, 10) + 17}%;"></div>`
+      this.redTarget.outerHTML = `<div class="red" data-game-target="red" style="width: ${100 - (parseInt(percent, 10) + 17)}%;"></div>`
+      this.percentTarget.innerHTML = `${parseInt(percent, 10) + 17}%`;
+    }
+  }
+
+  avatar(event) {
+    if (this.avatarTarget.matches(".false") && event.x < (window.innerWidth / 2)) {
+      this.avatarTarget.classList.add("true");
+      this.avatarTarget.classList.remove("false");
+      this.avatarTarget.innerHTML = '<i class="fas fa-check"></i>';
+      const regexp = /\d+/;
+      const percent = this.greenTarget.outerHTML.match(regexp)[0];
+      this.greenTarget.outerHTML = `<div class="green" data-game-target="green" style="width: ${parseInt(percent, 10) + 10}%;"></div>`
+      this.redTarget.outerHTML = `<div class="red" data-game-target="red" style="width: ${100 - (parseInt(percent, 10) + 10)}%;"></div>`
+      this.percentTarget.innerHTML = `${parseInt(percent, 10) + 10}%`;
+    }
+  }
+  contact(event) {
+    if (this.contactTarget.matches(".false") && (window.scrollY+100) > this.endTarget.offsetTop) {
+      this.contactTarget.classList.add("true");
+      this.contactTarget.classList.remove("false");
+      this.contactTarget.innerHTML = '<i class="fas fa-check"></i>';
+      const regexp = /\d+/;
+      const percent = this.greenTarget.outerHTML.match(regexp)[0];
+      this.greenTarget.outerHTML = `<div class="green" data-game-target="green" style="width: ${parseInt(percent, 10) + 14}%;"></div>`
+      this.redTarget.outerHTML = `<div class="red" data-game-target="red" style="width: ${100 - (parseInt(percent, 10) + 14)}%;"></div>`
+      this.percentTarget.innerHTML = `${parseInt(percent, 10) + 14}%`;
     }
   }
 
