c3e4fbe29e66b873b35b1e5f2d4b945416cf021e
remi <remi.tondeux@gmail.com>
2022-01-02 14:40:10 +0100

    game 45%

--- app/javascript/controllers/game_controller.js
+++ app/javascript/controllers/game_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ['hint','mode', 'avatar', 'card', 'tag', 'carrou', 'green', 'red']
+  static targets = ['hint','mode', 'avatar', 'card', 'tag', 'carrou', 'green', 'red', 'percent']
 
   hint(event) {
     this.hintTarget.classList.toggle("d-none");
@@ -12,7 +12,11 @@ export default class extends Controller {
       this.modeTarget.classList.add("true");
       this.modeTarget.classList.remove("false");
       this.modeTarget.innerHTML = '<i class="fas fa-check"></i>';
-      // this.greenTarget.outerHTML =
+      const regexp = /\d+/ ;
+      const percent = this.greenTarget.outerHTML.match(regexp)[0];
+      this.greenTarget.outerHTML = `<div class="green" data-game-target="green" style="width: ${parseInt(percent, 10) + 25}%;"></div>`
+      this.redTarget.outerHTML = `<div class="red" data-game-target="red" style="width: ${100-(parseInt(percent, 10) + 25)}%;"></div>`
+      this.percentTarget.innerHTML = `${parseInt(percent, 10)+25}%`;
     }
   }
 }
